917754977739dedf2beeb9cf01e030866a54d433
JoseCastillo242 <161986669+JoseCastillo242@users.noreply.github.com>
2024-10-22 18:16:35 -0600

    LA TABLA

--- Punto de equilibrio.py
+++ Punto de equilibrio.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 import matplotlib as mt
+from tkinter import ttk
 
 frame = tk.Tk()
 frame.title("Punto de Equilibrio")
@@ -20,9 +21,9 @@ def calcular():
     
         resulta = puntoequi(preven, costouni, gasfig)
         result.config(text= f"Para Alcanzar el punto de equilibrio deberia vender {resulta} unidades")
+        
         resultPlati = puntoPlatita(resulta, preven)
         resultadoPlati.config(text= f"La empresa tendria que vender Q{resultPlati} para estar en su punto de equilibrio")
-
         
     except ValueError:
         messagebox.showerror("", "El valor introducido no es valido, Introduce Por favor un Numero")
@@ -44,8 +45,21 @@ btncalc = tk.Button(frame, command=calcular, text="Calcular", font=("Times New R
 btncalc.pack()
 result = tk.Label(frame, text="", font=("Times New Roman", 10)) #muestra los resultados de las operaciones
 result.pack()
-
 resultadoPlati = tk.Label(frame, text="", font=("Times New Roman", 10)) #muestra la cantidad de quetzales que se tienen que vender
 resultadoPlati.pack()
+
+tree = ttk.Treeview(frame, columns=("Blank", "25PorMenos", "Punto", "25PorMas"), show="headings", height=5)
+tree.heading("Blank", text="")
+tree.heading("25PorMenos", text="25% menos de produccion")
+tree.heading("Punto", text="Punto de equilibrio")
+tree.heading("25PorMas", text="25% mas de produccion")
+tree.column("Blank", width=100)
+tree.column("25PorMenos", width=250)
+tree.column("Punto", width=120)
+tree.column("25PorMas", width=300)
+
+# Insertar datos en el Treeview
+tree.insert("", "end", values=("Christopher Ricardo Garcia Giron", "0907-24-10087", "Líder de proyecto, Encargado de la intefaz principal"))
+tree.pack(padx=10, pady=50)  # Usamos pack en lugar de grid para este caso
 
 frame.mainloop()
